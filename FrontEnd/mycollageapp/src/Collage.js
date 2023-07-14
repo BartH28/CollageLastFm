fb34b30073b2cd88303b61c87c1440ba56f02195
Henrique Barreto Leonardo <henrique.leonardo@intelitrader.com.br>
2023-07-14 17:11:07 -0300

    Collage separado em componente (incompleto)

--- FrontEnd/mycollageapp/src/Collage.js
+++ FrontEnd/mycollageapp/src/Collage.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
-export default function Collage() {
+export default function Collage({name}) {
+
+    const [weekAlbums, setWeekAlbums] = useState([])
+    const [name, setName] = useState("")
+    const api_key = 'eb89f11d64e5a3ccced854eb18c83825'
+
+    function getAlbumsWeeklyCharts(event) {
+    event.preventDefault()
+    
+    axios("http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user="+name +"&api_key="+api_key+"&format=json&period=7day&limit=25")
+      .then(r => {
+        setWeekAlbums(r.data.topalbums.album)
+        
+      })
+      .catch(err => console.log(err))
+    }
+
+
     return (
       <div>Collage</div>
     )
